2c73ecacdbac8665c1c2948675a91a6625680823
Alexandra <superbiscuit@gmail.com>
2017-05-12 11:53:38 -0700

    All problems, and verified they work

--- Euler1to5.js
+++ Euler1to5.js
@@ -9,7 +9,10 @@ function euler(){
 	return res 
 }
 
+//answer is 234168
+
 //Problem 2
+
 function evenFib(max){
 	var i = 1
 	var j = 2
@@ -25,6 +28,8 @@ function evenFib(max){
 	return res
 }
 
+evenFib(4000000)
+//answer 4613732
 
 //Problem 3
 
@@ -67,31 +72,87 @@ function largestPrime(num){
 }
 
 largestPrime(600851475143)
+//answer is 6857
 
-console.log(largestPrime(777))
 
 // problem 4
 
 function largestPalindrome(){
+	res = 0
 	for (let i = 100; i < 1000; i++){
-		for (let j = 100; j < 1000; i++){
-
+		for (let j = 100; j < 1000; j++){
+			var product = i * j;
+			if (palindrome(product) && product > res){
+				res = product
+			}
 		}
 	}
+	return res 
 }
 
 function palindrome(int){
-	numbers = int.toString()
-	if (numbers.length % 2 != 0){
-		return false
+	numbers = int.toString();
+	if (numbers.length % 2 !== 0){
+	  half = Math.ceil(numbers.length / 2);
+	  chunk1 = numbers.slice(0,half);
+	  chunk2 = numbers.slice(half - 1, numbers.length);
+	  return chunk1 === chunk2.split("").reverse().join("");
 	}
-	numbers.length / 2 = half
-	chunk1 = numbers.slice(0, half - 1) 
-	chunk2 =numbers.slice(half, numbers.length - 1)
-	return chunk1 === chunk2.reverse()
+	//wish this was ruby, sometimes you have to be obnoxiously verbose in javascript
+  half = numbers.length / 2;
+	chunk1 = numbers.slice(0, half); 
+	chunk2 =numbers.slice(half, numbers.length);
+	return chunk1 === chunk2.split("").reverse().join("");
 }
 
+largestPalindrome()
 
+//answer is 906609
+
+//problem 5
+
+function divCheck(arr, num){
+	arr = arr.reverse()
+	for (let i = 0; i < arr.length; i++){
+		if (num % arr[i] !== 0){
+			return false
+		}
+	}
+	return true
+}
+
+function range(int){
+  arr =[]
+  for (let i = 2; i <= int; i++){
+    arr.push(i)
+  }
+  return arr 
+}
+
+function euler(int){
+	numbers = range(int)
+	product =  numbers.reduce(function(a,b){return a*b;})
+	numbers = numbers.reverse()
+	condition = true
+	while(condition){ 
+	for (let i=0; i < numbers.length; i++){
+		temp = product / numbers[i]
+		console.log(temp)
+		if(divCheck(numbers,temp)){
+			product = temp
+			break;
+		}
+		if (i === numbers.length - 1){
+			return product
+		}
+	}
+	}
+	
+}
+
+euler(15)
+
+//answer is 23063040
 
 
 
